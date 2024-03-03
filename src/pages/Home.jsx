cc7aab3b2692f33773e37238fb09a0e8ed2a051e
Md Faizal <faizalmohammd67@gmail.com>
2024-03-03 17:58:50 +0530

    modify Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import appwriteService from '../appwrite/db'
 import { Container, PostCard } from '../components'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     const [posts, setPosts] = useState([])
@@ -12,6 +14,10 @@ const Home = () => {
             })
             .catch(console.log('Error occur at home'))
     }, [])
+
+const userStatus = useSelector((state)=> state.auth.status)
+
+!userStatus && <div><Container><h1>Please Login first to read the Blog</h1><Link to='/login'>Login</Link></Container></div>
 
     if (posts.length === 0)
         return (
